7bfa980092189d447e2e0edcc799cd7ff63ff7ac
IyinoluwAA <iyinoluwa150@gmail.com>
2025-03-11 02:53:40 +0100

    COMMIT - 1.5 Successfully created the error rust code manage errors in our application and successfully implemented the database layer for the rust application handling various user operation (db.rs file serves as the backbone of the database interaction and ensuring that data is managed efficientlty and securely) diving into setting up a web server using axum

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 mod models;
 mod config;
 mod dtos;
+mod error;
+mod db;
 
+use config::Config;
+use db::DBClient;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient,
+}
+
+#[tokio::main]
 fn main (){
     todo!()
 }
